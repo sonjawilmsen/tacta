622bfdf2b0291bfb6f15acad55d388c49ce89c1d
Sonja Wilmsen <s_wilmsen@hotmail.com>
2016-06-09 10:28:21 +0200

    loop and quit

--- tacta.rb
+++ tacta.rb
@@ -26,12 +26,16 @@ contacts << { name: "Nikola Tesla"    , phone: "+385 43 987 3355", email: "nik@i
 contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "contact@empire.com" }
 contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org"     }
 
-index(contacts)
+loop do
+  index(contacts)
 
-response = ask("Who would you like to see? ")
+  response = ask("Who would you like to see? (q to quit): ")
+  break if response == "q"
 
-i = response.to_i
+  i = response.to_i
 
-contact = contacts[i-1]
+  contact = contacts[i-1]
 
-show(contact)
+  show(contact)
+  puts
+end
