6aa7f4903c2ebd23cc8edf132879d4b869c0ed16
Sonja Wilmsen <s_wilmsen@hotmail.com>
2016-06-09 10:24:01 +0200

    Create methods for index, show, and ask

--- tacta.rb
+++ tacta.rb
@@ -1,3 +1,23 @@
+
+def index(contacts)
+  contacts.each_with_index do |contact, i|
+     puts "#{i+1}) #{contact[:name]}"
+  end
+end
+
+def show(contact)
+  puts
+  puts "#{contact[:name]}"
+  puts "phone: #{contact[:phone]}"
+  puts "email: #{contact[:email]}"
+end
+
+def ask(prompt)
+   puts
+   print prompt
+   gets.chomp
+end
+
 contacts = []
 
 contacts << { name: "Thomas Jefferson", phone: "+1 206 310 1369" , email: "tjeff@us.gov"       }
@@ -6,19 +26,12 @@ contacts << { name: "Nikola Tesla"    , phone: "+385 43 987 3355", email: "nik@i
 contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "contact@empire.com" }
 contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org"     }
 
-contacts.each_with_index do |contact, i|
-   puts "#{i+1}) #{contact[:name]}"
-end
+index(contacts)
 
-puts
-print "Who would you like to see? "
-response = gets.chomp
+response = ask("Who would you like to see? ")
 
 i = response.to_i
 
 contact = contacts[i-1]
 
-puts
-puts "#{contact[:name]}"
-puts "phone: #{contact[:phone]}"
-puts "email: #{contact[:email]}"
+show(contact)
