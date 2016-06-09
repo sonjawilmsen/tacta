cf19b8e088116db72e8493d6fbded135c05ea178
Sonja Wilmsen <s_wilmsen@hotmail.com>
2016-06-09 10:39:28 +0200

    create new method

--- tacta.rb
+++ tacta.rb
@@ -18,6 +18,21 @@ def ask(prompt)
    gets.chomp
 end
 
+def create_new
+   contact = {}
+
+   puts
+   puts "Enter contact info:"
+
+   contact[:name ] = ask "Name? "
+   contact[:phone] = ask "Phone? "
+   contact[:email] = ask "Email? "
+
+   contact
+end
+
+
+
 contacts = []
 
 contacts << { name: "Thomas Jefferson", phone: "+1 206 310 1369" , email: "tjeff@us.gov"       }
@@ -27,15 +42,32 @@ contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "conta
 contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org"     }
 
 loop do
-  index(contacts)
+   index( contacts )
 
-  response = ask("Who would you like to see? (q to quit): ")
-  break if response == "q"
+   puts
+   response = ask "Who would you like to see (n for new, q to quit)? "
 
-  i = response.to_i
+   break if response == "q"
 
-  contact = contacts[i-1]
+   if response == "n"
+     contact = create_new
 
-  show(contact)
-  puts
+     contacts << contact
+
+     puts
+     puts "New contact created:"
+     puts
+
+     show( contact )
+     puts
+   else
+      i = response.to_i
+
+      contact = contacts[i-1]
+
+      puts
+      show( contact )
+      puts
+   end
+
 end
